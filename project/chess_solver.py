8d0e43b5f3406db93c10abd27cce0701c2ce3580
mafer1 <ferenc312@gmail.com>
2023-02-15 20:52:52 +0100

    feat: add Pawn class

--- project/chess_solver.py
+++ project/chess_solver.py
@@ -19,3 +19,37 @@ class Figure(metaclass=ABC):
     def validate_move(self, dest_field: str) -> bool:
         """A function which validates correctness of move
         from current field to destination field for a defined chess figure"""
+
+
+class Pawn(Figure):
+    """Pawn figure - solider of the first line"""
+
+    def __init__(self, current_field):
+        self.current_field = current_field
+        self._tuple_position = (str(current_field[0]), int(current_field[1]))
+
+    def list_available_moves(self) -> list:
+        """
+        Pawns move only vertically forward one square.
+        ONE EXCEPTION - when pawn is not moved, it can move forward two squares.
+        :return: list
+        """
+        if self._tuple_position not in chess_board:
+            raise Exception("Field does not exist.")
+        elif self._tuple_position[1] == 2:
+            return [
+                f"{self._tuple_position[0]}{int(self._tuple_position[1]) + 1}",
+                f"{self._tuple_position[0]}{int(self._tuple_position[1]) + 2}",
+            ]
+        else:
+            return [f"{self._tuple_position[0]}{int(self._tuple_position[1]) + 1}"]
+
+    def validate_move(self, dest_field: str) -> bool:
+        """
+        Move validator for Pawn figure. This figure moves only in one direction.
+        If current position equals 2 - it is initial position
+        Else: Pawn was already moved.
+        :param dest_field:
+        :return: bool
+        """
+        return True if dest_field in self.list_available_moves() else False
